43a0796d712394b1ecc5d695c38d79d9e5e778de
ConstintCreations <ethanglenn1208@gmail.com>
2025-11-22 16:46:17 -0600

    Started Goals Page

---
+++ app/components/goalGrid.tsx
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+export default function GoalGrid() {
+    return (
+        <div className="flex flex-row flex-wrap gap-6 justify-center">
+            
+            <a href="/goals/create" className="cursor-pointer flex flex-col justify-center items-center h-40 w-40 bg-gray-800 hover:bg-gray-700 rounded-3xl border-5 border-gray-700 hover:border-gray-600 text-gray-400 hover:text-gray-300 transition-colors ease-in-out duration-300">
+                <FontAwesomeIcon icon={faPlus} className="h-20"></FontAwesomeIcon>
+            </a>
+        </div>
+    )
+}
--- app/goals/page.tsx
+++ app/goals/page.tsx
@@ -1,7 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBullseye } from "@fortawesome/free-solid-svg-icons";
+import GoalGrid from "../components/goalGrid";
+
 export default function Goals() {
     return (
-        <div>
-            <h1>Goals Page</h1>
+        <div className="flex flex-col items-center justify-center mt-10 px-10 gap-10">
+            <h1 className="flex flex-row gap-2 items-center text-3xl self-start text-gray-300"><FontAwesomeIcon icon={faBullseye} className="h-8"></FontAwesomeIcon>My Goals</h1>
+            <GoalGrid>
+
+            </GoalGrid>
         </div>
     )
 }
