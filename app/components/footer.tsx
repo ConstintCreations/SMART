a9e0185d369a57a67d238ba6a1114deabd06248a
ConstintCreations <ethanglenn1208@gmail.com>
2025-11-22 16:29:55 -0600

    Added Header Links, added FontAwesome icons, and updated footer link hover.

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="w-full h-14 flex-row bg-gray-900 flex flex-row justify-center items-center fixed bottom-0">
-            <p>Created by ConstintCreations | Source on <a className="text-blue-400" href="https://github.com/ConstintCreations/SMART">Github</a></p>
+            <p>Created by ConstintCreations | Source on <a className="text-blue-400 hover:border-b hover:border-blue-400" href="https://github.com/ConstintCreations/SMART">Github</a></p>
         </footer>
     )
 }
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,13 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBullseye } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 export default function Header() {
     return (
-        <header className="w-full h-14 flex flex-row items-center bg-gray-900 sticky top-0">
-            <a href="/" className="flex flex-row text-3xl font-bold gap-1 ml-10 cursor-pointer hover:translate-y-[-3px] transition-transform ease-in-out duration-300">
+        <header className="w-full h-14 flex flex-row justify-between items-center bg-gray-900 sticky top-0 px-10">
+            <a href="/" className="flex flex-row text-3xl font-bold gap-1 cursor-pointer hover:translate-y-[-3px] transition-transform ease-in-out duration-300">
                 <p className="text-red-500">S</p>
                 <p className="text-orange-500">M</p>
                 <p className="text-yellow-500">A</p>
                 <p className="text-green-500">R</p>
                 <p className="text-blue-500">T</p>
             </a>
+            <div className="flex flex-row gap-6">
+                <a href="/goals" className="text-gray-400 hover:text-gray-300 group transition-colors ease-in-out duration-300 flex flex-row gap-2 items-center"><FontAwesomeIcon icon={faBullseye} className="h-5"></FontAwesomeIcon><div className="border-b border-gray-400 group-hover:border-gray-300 transition-colors ease-in-out duration-300">My Goals</div></a>
+                <a href="/goals/create" className="text-gray-400 hover:text-gray-300 group transition-colors ease-in-out duration-300 flex flex-row gap-2 items-center"><FontAwesomeIcon icon={faPlus} className="h-5"></FontAwesomeIcon><div className="border-b border-gray-400 group-hover:border-gray-300 transition-colors ease-in-out duration-300">New Goal</div></a>
+            </div>
         </header>
     );
 }
