206af5c03770cb1ebadf0b9e0212938e8c4ac4fe
ConstintCreations <ethanglenn1208@gmail.com>
2025-11-22 20:26:44 -0600

    Goal Card UI

--- app/components/goalCard.tsx
+++ app/components/goalCard.tsx
@@ -1,7 +1,29 @@
-export default function GoalCard() {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faEdit, faCheck } from "@fortawesome/free-solid-svg-icons";
+
+export default function GoalCard({ text, colorID }: { text: string, colorID: number }) {
+    const colors = [
+        "bg-red-900 hover:bg-red-800 border-red-700 hover:border-red-600",
+        "bg-orange-900 hover:bg-orange-800 border-orange-700 hover:border-orange-600",
+        "bg-yellow-900 hover:bg-yellow-800 border-yellow-700 hover:border-yellow-600",
+        "bg-green-900 hover:bg-green-800 border-green-700 hover:border-green-600",
+        "bg-blue-900 hover:bg-blue-800 border-blue-700 hover:border-blue-600",
+    ]
+
     return (
-        <div>
+        <div className={`relative cursor-pointer px-5 py-3 text-center flex flex-col justify-center items-center h-40 w-80 ${colors[colorID]} rounded-3xl border-5 text-gray-300 transition-colors ease-in-out duration-300 overflow-hidden group`}>
             
+            <div className="flex flex-row gap-1 absolute top-3 left-0 w-full justify-between px-3 opacity-0 group-hover:opacity-100 transition-opacity ease-in-out duration-300">
+                <FontAwesomeIcon className="size-8 text-red-400 bg-gray-800 p-1.5 rounded-lg hover:bg-gray-700 transition-colors ease-in-out duration-300" icon={faTrash}></FontAwesomeIcon>
+                <div className="flex flex-row gap-1">
+                    <FontAwesomeIcon className="size-8 text-gray-400 bg-gray-800 p-1.5 rounded-lg hover:bg-gray-700 transition-colors ease-in-out duration-300" icon={faEdit}></FontAwesomeIcon>
+                    <FontAwesomeIcon className="size-8 text-green-400 bg-gray-800 p-1.5 rounded-lg hover:bg-gray-700 transition-colors ease-in-out duration-300" icon={faCheck}></FontAwesomeIcon>
+                </div>
+            </div>
+            
+            <p className="text-sm font-bold h-40 w-full flex flex-col justify-center items-center overflow-hidden">
+                {text}
+            </p>
         </div>
     )
 }
--- app/components/goalGrid.tsx
+++ app/components/goalGrid.tsx
@@ -1,10 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import GoalCard from "./goalCard";
 
 export default function GoalGrid() {
     return (
-        <div className="flex flex-row flex-wrap gap-6 justify-center">
-            
+        <div className="flex flex-row flex-wrap gap-6 justify-center items-center">
+            <GoalCard text={"the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never the end is never"} colorID={1}></GoalCard>
             <a href="/goals/create" className="cursor-pointer flex flex-col justify-center items-center h-40 w-40 bg-gray-800 hover:bg-gray-700 rounded-3xl border-5 border-gray-700 hover:border-gray-600 text-gray-400 hover:text-gray-300 transition-colors ease-in-out duration-300">
                 <FontAwesomeIcon icon={faPlus} className="h-20"></FontAwesomeIcon>
             </a>
