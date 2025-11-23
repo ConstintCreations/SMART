c69504b5242dfaaf04be65af7e24a90b7a416838
ConstintCreations <ethanglenn1208@gmail.com>
2025-11-23 15:20:02 -0600

    Fixed goal deletion issues

--- app/components/goalGrid.tsx
+++ app/components/goalGrid.tsx
@@ -102,7 +102,7 @@ export default function GoalGrid() {
     return (
         <div className="flex flex-row flex-wrap gap-6 justify-center items-center">
             {existingGoals.map((goal, index) => (
-                <GoalCard key={index} id={goal.id} text={goal.text} colorID={goal.colorID} onDelete={deleteGoal} onEdit={editGoal}></GoalCard>
+                <GoalCard key={goal.id} id={goal.id} text={goal.text} colorID={goal.colorID} onDelete={deleteGoal} onEdit={editGoal}></GoalCard>
             ))}
             <a href="/goals/create" className="cursor-pointer flex flex-col justify-center items-center h-40 w-40 bg-gray-800 hover:bg-gray-700 rounded-3xl border-5 border-gray-700 hover:border-gray-600 text-gray-400 hover:text-gray-300 transition-colors ease-in-out duration-300">
                 <FontAwesomeIcon icon={faPlus} className="!size-20"></FontAwesomeIcon>
