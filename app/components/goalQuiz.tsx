b86ddacc72fedb6f0f89212d31b42c9b39866e6f
ConstintCreations <ethanglenn1208@gmail.com>
2025-11-23 18:44:06 -0600

    Added suggestions for goals and ability to randomize goal segments

--- app/components/goalQuiz.tsx
+++ app/components/goalQuiz.tsx
@@ -1,8 +1,10 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDice } from "@fortawesome/free-solid-svg-icons";
 export default function GoalQuiz({ answerSlots, setAnswerSlots, setQuizComplete }: { answerSlots: string[], setAnswerSlots: (answers: string[]) => void, setQuizComplete: (value: boolean) => void }) {
 
-    let colors = {
+    const colors = {
         Specific: "bg-red-500",
         Measurable: "bg-orange-500",
         Achievable: "bg-yellow-500",
@@ -17,6 +19,7 @@ export default function GoalQuiz({ answerSlots, setAnswerSlots, setQuizComplete 
         stem: string;
         spot: number;
         date?: boolean;
+        suggestions?: string[];
     };
 
     const questions:Question[] = [
@@ -24,13 +27,15 @@ export default function GoalQuiz({ answerSlots, setAnswerSlots, setQuizComplete 
             category: "Specific",
             question: "What do you want to achieve?",
             stem: "I want to",
-            spot: 0
+            spot: 0,
+            suggestions: ["learn how to play the guitar", "run a marathon", "read 3 books", "lose 10 pounds", "start a blog", "improve my public speaking skills", "learn a new language", "build a mobile app", "meditate daily", "cook healthy meals", "learn web development", "improve my photography skills"]
         },
         {
             category: "Relevant",
             question: "Why do you want to achieve this goal?",
             stem: "Because",
-            spot: 5
+            spot: 5,
+            suggestions: ["it will improve my self-image", "it would be a good challenge", "it would help solve a problem", "I've always wanted to do it", "it aligns with my values", "it will enhance my career prospects", "it will improve my health", "it will boost my confidence", "it will allow me to connect with others", "it will help me develop new skills"]
         },
         {
             category: "Time-Bound",
@@ -43,19 +48,22 @@ export default function GoalQuiz({ answerSlots, setAnswerSlots, setQuizComplete 
             category: "Achievable",
             question: "How will you achieve this goal?",
             stem: "By",
-            spot: 2
+            spot: 2,
+            suggestions: ["practicing", "taking classes", "hiring a coach", "setting a schedule", "breaking it into smaller tasks", "following a plan"]
         },
         {
             category: "Measurable",
             question: "How often will you work on this goal?",
             stem: "Every",
-            spot: 3
+            spot: 3,
+            suggestions: ["day", "week", "other day", "Monday, Wednesday, and Friday", "morning", "evening", "weekend", "month", "2 weeks", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "weekday"]
         },
         {
             category: "Measurable",
             question: "For how long will you work on this goal each time?",
             stem: "For",
-            spot: 4
+            spot: 4,
+            suggestions: ["30 minutes", "1 hour", "2 hours", "15 minutes", "the duration of my lunch break", "3 hours", "45 minutes", "5 hours", "10 minutes", "90 minutes", "5 minutes", "4 hours"]
         }
     ]
 
@@ -78,6 +86,16 @@ export default function GoalQuiz({ answerSlots, setAnswerSlots, setQuizComplete 
             }
         }
     };
+
+    const [randomPlaceholder, setRandomPlaceholder] = useState("");
+
+    useEffect(() => {
+        const suggestions = questions[currentQuestionIndex].suggestions;
+        if (suggestions && suggestions.length > 0) {
+            const randomIndex = Math.floor(Math.random() * suggestions.length);
+            setRandomPlaceholder(suggestions[randomIndex]);
+        }
+    }, [currentQuestionIndex]);
 
     return (
         <div className="w-full max-w-4xl bg-gray-800 rounded-lg px-10 py-6 flex flex-col items-center">
@@ -112,14 +130,28 @@ export default function GoalQuiz({ answerSlots, setAnswerSlots, setQuizComplete 
                                 }}>
                             </input>
                         </div> : 
-                        <input type="text" className="bg-gray-800 text-gray-300 rounded-lg px-3 py-2 border-2 border-gray-600 focus:border-blue-500 focus:bg-blue-900 outline-none transition-all ease-in-out duration-300 text-2xl w-100" 
-                            value={answerSlots[questions[currentQuestionIndex].spot]}
-                            onChange={(e) => {
-                                const newAnswers = [...answerSlots];
-                                newAnswers[questions[currentQuestionIndex].spot] = e.target.value;
-                                setAnswerSlots(newAnswers);
-                            }}
-                        ></input>
+                        <span className="flex flex-row items-center">
+                            <input type="text" className="bg-gray-800 text-gray-300 rounded-lg px-3 py-2 border-2 border-gray-600 hover:border-gray-500 hover:bg-gray-700 focus:border-blue-500 focus:bg-blue-900 outline-none transition-all ease-in-out duration-300 text-2xl w-100" 
+                                value={answerSlots[questions[currentQuestionIndex].spot]}
+                                placeholder={randomPlaceholder}
+                                onChange={(e) => {
+                                    const newAnswers = [...answerSlots];
+                                    newAnswers[questions[currentQuestionIndex].spot] = e.target.value;
+                                    setAnswerSlots(newAnswers);
+                                }}
+                            ></input>
+                            <FontAwesomeIcon icon={faDice} tabIndex={0} className="!size-8 text-gray-400 border-2 border-gray-600 bg-gray-800 hover:border-blue-500 hover:bg-blue-900 p-2 rounded-xl ml-2 cursor-pointer hover:text-gray-300 transition-colors ease-in-out duration-300" onClick={() => {
+                                    const suggestions = questions[currentQuestionIndex].suggestions;
+                                    if (suggestions && suggestions.length > 0) {
+                                        const randomIndex = Math.floor(Math.random() * suggestions.length);
+                                        const newAnswers = [...answerSlots];
+                                        newAnswers[questions[currentQuestionIndex].spot] = suggestions[randomIndex];
+                                        setAnswerSlots(newAnswers);
+                                    }
+                                }
+                            }></FontAwesomeIcon>
+                        </span>
+                        
                     }
                 </div>
 
